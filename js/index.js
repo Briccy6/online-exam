0763da0a307addfc015d8f5ea03348e151bfa89c
Briccy6 <hirwabrian9@gmail.com>
2025-02-07 15:18:02 +0200

    initial commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// function product
+// (a,d){
+//     return a*d;
+// }
+// var total=product(2,6)
+// console .log (total);
+// function welcome(y){
+// y="welcome sir/madam";
+// console.log(y);
+// }
+// welcome();
+// var welcome = greetings => {
+//     console.log(greetings);
+//     }
+//     welcome("Rosine");
+structuredClone()
